896db6c70c4d86dc717b863b62e5f4c909473a1a
StressedBy <150947532+StressedBy@users.noreply.github.com>
2023-12-11 15:28:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function show_sidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display= 'flex'
+}
+function hide_sidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display= 'none'
+}
+
+function show_popup(){
+    const register_popup = document.querySelector('.register_popup')
+    .register_popup.style.display = 'flex'
+}
